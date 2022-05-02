cfecbbe572bb30201c40240a46f8d4d4814d0374
ParthaManna <manna79partha@gmail.com>
2022-05-02 21:22:08 +0530

    views updated

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,13 +2,10 @@ from django.shortcuts import render
 # rest_framework imports
 from rest_framework import status
 from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 #models & serializers import
 from .models import TODO
 from .serializers import TODOSerializer
-from todoapp import serializers
 
 
 class ToDoView(generics.ListCreateAPIView):    #for viewing & adding todo contents
